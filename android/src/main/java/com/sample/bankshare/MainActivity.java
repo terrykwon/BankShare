dc35a667d0ae55e11cd5f6e9465eda31fc4a1b91
terrykwon <terrykkwon@gmail.com>
2017-03-19 20:42:32 +0900

    Add basic layout for MainActivity

--- android/src/main/java/com/sample/bankshare/MainActivity.java
+++ android/src/main/java/com/sample/bankshare/MainActivity.java
@@ -2,12 +2,22 @@ package com.sample.bankshare;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView mRecycler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Set up toolbar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_main);
+        setSupportActionBar(toolbar);
+
+        mRecycler = (RecyclerView) findViewById(R.id.recycler_main_rooms);
     }
 }
