f99f5b5376b0714f02d12840dce17055607e9693
terrykwon <terrykkwon@gmail.com>
2017-03-20 12:59:16 +0900

    Add user list to drawer

--- android/src/main/java/com/sample/bankshare/account/AccountActivity.java
+++ android/src/main/java/com/sample/bankshare/account/AccountActivity.java
@@ -3,12 +3,18 @@ package com.sample.bankshare.account;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 
 import com.sample.bankshare.R;
+import com.sample.bankshare.util.DummyGenerator;
 
 public class AccountActivity extends AppCompatActivity {
     private DrawerLayout mDrawerLayout;
+    private RecyclerView mUserRecycler, mAccountRecycler;
+    private UserAdapter mUserAdapter;
+    private LinearLayoutManager mUserLayoutManager, mAccountLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +24,14 @@ public class AccountActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_account);
         setSupportActionBar(toolbar);
 
+        mUserRecycler = (RecyclerView) findViewById(R.id.recycler_account_drawer_members);
+        mUserAdapter = new UserAdapter();
+        mUserLayoutManager = new LinearLayoutManager(this);
+
+        mUserRecycler.setLayoutManager(mUserLayoutManager);
+        mUserRecycler.setAdapter(mUserAdapter);
+
+        // Debug
+        mUserAdapter.setUserList(DummyGenerator.generateDummyUsers());
     }
 }
