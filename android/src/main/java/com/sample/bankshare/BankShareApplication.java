312b390096a29544cc5c6ccb95b0b68b4eee5011
jeonghanjoo <joo3436@gmail.com>
2017-03-25 23:23:02 +0900

    Add Kakao Login

---
+++ android/src/main/java/com/sample/bankshare/BankShareApplication.java
@@ -0,0 +1,88 @@
+package com.sample.bankshare;
+
+import android.app.Activity;
+import android.app.Application;
+import android.content.Context;
+
+import com.kakao.auth.ApprovalType;
+import com.kakao.auth.AuthType;
+import com.kakao.auth.IApplicationConfig;
+import com.kakao.auth.ISessionConfig;
+import com.kakao.auth.KakaoAdapter;
+import com.kakao.auth.KakaoSDK;
+
+/**
+ * Created by jj on 25/03/2017.
+ */
+
+public class BankShareApplication extends Application {
+
+    private static Application instance;
+
+    private static class KakaoSDKAdapter extends KakaoAdapter {
+        /**
+         * Session Config에 대해서는 default값들이 존재한다.
+         * 필요한 상황에서만 override해서 사용하면 됨.
+         * @return Session의 설정값.
+         */
+        @Override
+        public ISessionConfig getSessionConfig() {
+            return new ISessionConfig() {
+                @Override
+                public AuthType[] getAuthTypes() {
+                    return new AuthType[] {AuthType.KAKAO_LOGIN_ALL};
+                }
+
+                @Override
+                public boolean isUsingWebviewTimer() {
+                    return false;
+                }
+
+                @Override
+                public boolean isSecureMode() {
+                    return false;
+                }
+
+                @Override
+                public ApprovalType getApprovalType() {
+                    return ApprovalType.INDIVIDUAL;
+                }
+
+                @Override
+                public boolean isSaveFormData() {
+                    return true;
+                }
+            };
+        }
+
+        @Override
+        public IApplicationConfig getApplicationConfig() {
+            return new IApplicationConfig() {
+                @Override
+                public Context getApplicationContext() {
+                    return BankShareApplication.getGlobalApplicationContext();
+                }
+            };
+        }
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        instance = this;
+
+        KakaoSDK.init(new KakaoSDKAdapter());
+    }
+
+    public static Application getGlobalApplicationContext() {
+        if(instance == null)
+            throw new IllegalStateException("this application does not inherit com.kakao.GlobalApplication");
+        return instance;
+    }
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+        instance = null;
+    }
+}
